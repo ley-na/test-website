3b0f9c977304109b158b87736de8ed8e53603871
Elena Voytovich <elena-voytovich@yandex.ru>
2022-01-20 13:52:23 +0300

    feat: implement flipping cards

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const menuToggle = document.querySelector(".nav__toggle");
 const navMenu = document.querySelector(".nav__list");
-const navLink = document.querySelectorAll(".nav__link");
+const navLinks = document.querySelectorAll(".nav__link");
+const cardButtons = document.querySelectorAll(".card__cta-btn");
 
 menuToggle.addEventListener("click", changeMenuState);
 
-navLink.forEach(link => link.addEventListener("click", changeMenuState));
+navLinks.forEach(link => link.addEventListener("click", changeMenuState));
 
 function changeMenuState() {
   menuToggle.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
 
+cardButtons.forEach(button => button.addEventListener("click", () => {
+  const card = button.closest(".card__container");
+  card.classList.toggle("flipped");
+}))
